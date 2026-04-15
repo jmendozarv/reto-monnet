6b2d925d52e99330aa40e7ce2b4e938ed265ff06
jmendozarv <52211870+jmendozarv@users.noreply.github.com>
2026-04-14 21:31:34 -0500

    add demo user

---
+++ channel-demo-users/src/main/java/com/challenger/channel_demo_users/ChannelDemoUsersApplication.java
@@ -0,0 +1,13 @@
+package com.challenger.channel_demo_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelDemoUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelDemoUsersApplication.class, args);
+	}
+
+}
---
+++ channel-demo-users/src/test/java/com/challenger/channel_demo_users/ChannelDemoUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenger.channel_demo_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelDemoUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
